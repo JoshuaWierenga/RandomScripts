b7ffb4204a8824057c8498cf9ab2c00bb104ed85
Joshua Wierenga <joshua.wierenga.student@gmail.com>
2023-08-22 00:37:22 +1000

    Rename toolchain files
    Previously they ended in .com which Windows glossed over when compile.com asks for a binary without it but other oses do not.

--- buildcosmo.sh
+++ buildcosmo.sh
@@ -13,6 +13,10 @@ if [ ! -f o/third_party/gcc ]; then
   curl -LO https://github.com/ahgamut/musl-cross-make/releases/download/z0.0.0/gcc11.zip
   tar -xvf gcc11.zip
   rm gcc11.zip
+  
+  # Rename toolchain programs to not have .com suffix
+  # From https://unix.stackexchange.com/a/659132
+  find bin -type f -name '*.com' -exec sh -c 'mv -- "$1" "${1%.*}"' sh {} \;
   )
 fi
 
