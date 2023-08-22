9912822ff28f801da2fd0b19e83acfdd8e7334ec
Joshua Wierenga <joshua.wierenga.student@gmail.com>
2023-08-22 01:14:54 +0000

    Fix bsd check

--- buildcosmo.sh
+++ buildcosmo.sh
@@ -4,7 +4,7 @@ portable_nproc() {
   OS="$(uname -s)"
   if [ "$OS" = "Linux" ]; then
     NPROCS="$(nproc --all)"
-  elif [ "$OS" = "Darwin" ] || [ "$(echo "$OS" | grep -q BSD)" = "BSD" ]; then
+  elif [ "$OS" = "Darwin" ] || [ -n "$(echo "$OS" | grep BSD)" ]; then
     NPROCS="$(sysctl -n hw.ncpu)"
   else
     NPROCS="$(getconf _NPROCESSORS_ONLN)"  # glibc/coreutils fallback
