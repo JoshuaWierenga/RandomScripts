a81d61452b88ba1e549efdc7ae5d14503af90ba5
Joshua Wierenga <joshua.wierenga.student@gmail.com>
2023-08-27 09:05:45 +0000

    Update buildapegcc.sh

--- buildapegcc.sh
+++ buildapegcc.sh
@@ -1,26 +1,24 @@
 #!/bin/sh
 
-# Joshua's ape gcc build script 0.2.1
+# Joshua's ape gcc build script 0.3.0
 # Based on https://github.com/ahgamut/musl-cross-make/blob/cibuild/.github/workflows/release.yml
 # Changes
-#0.2:   Reverted to an old version of cosmo that has been tested for building gcc
-#       will move back to a newer version once I get that working properly
+#0.3.0: Use a newer version of cosmo from 2023/08/12 instead of one from 2023/07/25
+#       Cleanup cosmo download code slightly but not using a subshell
 #0.2.1: Using same cosmo commit but now from the main repo, manually add getopt change
+#0.2.0: Reverted to an old version of cosmo that has been tested for building gcc
+#       will move back to a newer version once I get that working properly
 
 git clone https://github.com/ahgamut/musl-cross-make.git --depth=1 --branch z0.0.0
 cd musl-cross-make || exit
 
 mkdir cosmopolitan
-(
 cd cosmopolitan || exit
 git init
 git remote add origin https://github.com/jart/cosmopolitan.git
-git fetch origin 6843150e0c6322f50cda0ce59cca0b2e7397a3a7 --depth=1
+git fetch origin 8fc778162ecd227c66cac0333cdb66bdcec9662c --depth=1
 git reset --hard FETCH_HEAD
-)
-if [ ! -d cosmopolitan ]; then
-  exit
-fi
+cd ..
 
 cosmopolitan/ape/apeinstall.sh
 sed -i '7 i\
