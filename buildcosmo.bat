13f512d467e19c571e5ebf0ad212559bbbd1e5d0
Joshua Wierenga <joshua.wierenga.student@gmail.com>
2023-09-10 06:36:02 +0000

    Update buildcosmo.bat

--- buildcosmo.bat
+++ buildcosmo.bat
@@ -1,7 +1,9 @@
 @echo off
 
-REM Joshua's windows cosmopolitan build script 0.2.1
+REM Joshua's windows cosmopolitan build script 0.3.0
 REM Changes:
+REM 0.3.0: Use alternative musl toolchain prefix for cosmocc compat, transparently pass-through
+REM        parameters to make and remove now unneeded second make step for python tests
 REM 0.2.1: Use a version of ape gcc based on a much newer version of cosmo
 REM 0.2.0: Worked around python test flakiness and cosmo expecting toolchain binaries with
 REM        a specific prefix
@@ -19,24 +21,20 @@ if not exist o\third_party\gcc (
   del gcc11.zip
   
   REM Create versions of toolchain programs with the correct prefix and no .com suffix
-  REM Cosmo's build system expects x86_64-pc-linux-gnu-{toolname} while the tools
+  REM Cosmo's build system expects x86_64-linux-musl-{toolname} while the tools
   REM themselves just want {toolname} so this line can't just rename the files
   REM The orginal tools themselves having .com isn't actually a problem on windows as if
   REM {toolname} doesn't exist but {toolname}.com does then windows uses that. I am not
   REM entirely sure why though, perhaps a leftover from dos?
-  for /R bin %%x in (*.com) do copy "%%x" "bin\x86_64-pc-linux-gnu-%%~nx" >NUL
+  for /R bin %%x in (*.com) do copy "%%x" "bin\x86_64-linux-musl-%%~nx" >NUL
   cd ..\..\..
 )
 
-set PATHBACKUP=%PATH%
+setlocal
 set PATH=%PATH%;%CD%\o\third_party\gcc\bin
 
 REM %NUMBER_OF_PROCESSORS% is probably fine but whatever
 set /a "nproc=%NUMBER_OF_PROCESSORS%+1"
-build\bootstrap\make.com MODE= -j%nproc%
-REM Python tests are a bit flaky at higher job counts, 12 is fine on my 16 smt thread cpu
-REM No clue if 5 could be too high for some systems, ideally this could be removed
-build\bootstrap\make.com MODE= -j5 o//third_party/python
+build\bootstrap\make.com MODE= -j%nproc% %*
 
-set PATH=%PATHBACKUP%
-cd ..
+endlocal
