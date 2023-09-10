81e87d51dfc9bcfbbf0fcbaa748d65016e5c9ef5
Joshua Wierenga <joshua.wierenga.student@gmail.com>
2023-09-10 09:58:40 +0000

    Update cosmocc.bat

--- cosmocc.bat
+++ cosmocc.bat
@@ -1,29 +1,40 @@
 @echo off
 
-REM Joshua's windows cosmocc wrapper script 0.1.0
+REM Joshua's windows cosmocc wrapper script 0.1.1
+REM Changes:
+REM 0.1.1: Cleaned up the instructions and fixed a bug with endlocal
 
-
+goto: instend:
 REM Instructions:
-REM Replace SOMEPATH with wherever you put buildcosmo.bat, remove it and the \ if using the same directory
-REM 
+REM Download both buildcosmo.bat and cosmocc.bat and place in the same directory
+REM Putting the cosmopolitan and cosmos directories in different locations does work but isn't recommended
+REM
 REM To setup cosmocc:
-REM SOMEPATH\buildcosmo.bat o//examples/unbourne.com o//third_party/sed/sed.com o//tool/build/mv.com 
-REM set COSMO=SOMEPATH\cosmopolitan
+REM .\buildcosmo.bat o//examples/unbourne.com o//third_party/sed/sed.com o//tool/build/mv.com
+REM if using cmd run set COSMO=%cd%\cosmopolitan, if using pwsh run $env:COSMO="$PWD\cosmopolitan"
 REM .\cosmocc.bat --update
 REM
 REM To build an example program:
-REM Run whole thing including the blank line at the end
-REM echo #include ^<stdio.h^>^
-REM 
-REM ^
-REM 
-REM int main() {^
-REM 
-REM   printf("hello world\n");^
-REM 
-REM }> hello.c
-REM
+REM if using cmd run this whole thing including the blank line at the end
+echo #include ^<stdio.h^>^
+
+^
+
+int main() {^
+
+  printf("hello world\n");^
+
+}> hello.c
+
+REM if using pwsh then run
+Set-Content hello.c "#include <stdio.h>
+
+int main() {
+  printf(`"hello world\n`");
+}"
+REM finally run:
 REM .\cosmocc.bat -o hello.com hello.c
+:instend
 
 if not defined COSMO (
   exit /b 1
@@ -40,8 +51,9 @@ set PATH=%COSMO%\bin;%COSMO%\build\bootstrap;%COSMO%\cosmopolitan\o\examples;%CO
 
 REM %NUMBER_OF_PROCESSORS% is probably fine but whatever
 set /a "nproc=%NUMBER_OF_PROCESSORS%+1"
-endlocal
 
 "%COSMO%\o\third_party\sed\sed.com" -i ".bak" -E -e "s/^( *)((cat)|(mv))/\1cocmd.com -- \2/" -e "s/make --silent -j/make.com  --silent -j%nproc%/" -e "s/mkdir /mkdir.com /" "%COSMO%/bin/cosmocc"
 "%COSMO%\o\examples\unbourne.com" "%COSMO%\bin\cosmocc" %*
 move "%COSMO%\bin\cosmocc.bak" "%COSMO%\bin\cosmocc" >nul
+
+endlocal
