355fe0eea047914aa7eada30e7d332f336743e4b
Joshua Wierenga <joshua.wierenga.student@gmail.com>
2023-09-10 12:52:34 +0000

    Update buildcosmo.bat

--- buildcosmo.bat
+++ buildcosmo.bat
@@ -1,7 +1,10 @@
 @echo off
 
-REM Joshua's windows cosmopolitan build script 0.3.0
+REM Joshua's windows cosmopolitan build script 0.4.0
 REM Changes:
+REM 0.4.0: Moved zip download and extraction to a function and use in case git is unavailable
+REM        Also sped up git clone, copied toolchain check from unix shell version and ensure
+REM        calling shell's current directory is unchanged
 REM 0.3.0: Use alternative musl toolchain prefix for cosmocc compat, transparently pass-through
 REM        parameters to make and remove now unneeded second make step for python tests
 REM 0.2.1: Use a version of ape gcc based on a much newer version of cosmo
@@ -9,16 +12,22 @@ REM 0.2.0: Worked around python test flakiness and cosmo expecting toolchain bin
 REM        a specific prefix
 
 if not exist cosmopolitan (
-  git clone -c core.autocrlf=false https://github.com/JoshuaWierenga/cosmopolitan -b build-on-windows-3
+  where git >nul
+  if %ERRORLEVEL% equ 0 (
+    git clone -c core.autocrlf=false https://github.com/JoshuaWierenga/cosmopolitan --depth=1 -b build-on-windows-3
+  ) else (
+    call :download_zip https://github.com/JoshuaWierenga/cosmopolitan/archive/refs/heads/build-on-windows-3.zip,^
+    build-on-windows-3.zip, cosmopolitan-build-on-windows-3, cosmopolitan
+  )
 )
+setlocal
 cd cosmopolitan
 
-if not exist o\third_party\gcc (
+if not exist o\third_party\gcc\bin\x86_64-linux-musl-gcc (
   mkdir o\third_party\gcc
   cd o\third_party\gcc
-  curl -LO https://github.com/JoshuaWierenga/RandomScripts/releases/download/z0.0.0-1/gcc11.zip
-  tar -xvf gcc11.zip
-  del gcc11.zip
+  call :download_zip https://github.com/JoshuaWierenga/RandomScripts/releases/download/z0.0.0-1/gcc11.zip,^
+  gcc11.zip
   
   REM Create versions of toolchain programs with the correct prefix and no .com suffix
   REM Cosmo's build system expects x86_64-linux-musl-{toolname} while the tools
@@ -30,11 +39,18 @@ if not exist o\third_party\gcc (
   cd ..\..\..
 )
 
-setlocal
 set PATH=%PATH%;%CD%\o\third_party\gcc\bin
 
 REM %NUMBER_OF_PROCESSORS% is probably fine but whatever
 set /a "nproc=%NUMBER_OF_PROCESSORS%+1"
-build\bootstrap\make.com MODE= -j%nproc% %*
+call build\bootstrap\make.com MODE= -j%nproc% %*
 
 endlocal
+exit /b %ERRORLEVEL%
+
+:download_zip
+call curl -LO %~1
+call tar -xvf "%~2"
+del "%~2"
+if not "%~3"=="" ren "%~3" "%~4"
+exit /b 0
