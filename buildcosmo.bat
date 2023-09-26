f7fb127450e332b783fc46ea039ea312f36da9af
Joshua Wierenga <joshuagit@joshuawierenga.tech>
2023-09-26 17:05:03 +1000

    Update cosmo build scripts

--- buildcosmo.bat
+++ buildcosmo.bat
@@ -1,21 +1,22 @@
 @echo off
 
 REM NOTE: Until I fully test things, I recommend using 0.5.0 which uses the build-on-windows-3 branch
-REM Joshua's windows cosmopolitan build script 0.5.2
+REM Joshua's windows cosmopolitan build script 0.5.3
 REM Changes:
+REM 0.5:3: Improve copy check in download function and hide excessive output when extracting comso.
 REM 0.5.2: Updated apegcc version. The special handling for .com files has been removed and so
-REM        the extracted binaries are renamed just like the unix shell version of the script
+REM        the extracted binaries are renamed just like the unix shell version of the script.
 REM 0.5.1: Using experimental updated cosmo version, it fails more often but most of that
-REM        is from improved error detection triggering on issues that were already present
-REM 0.5.0: Updated apegcc version and prevent displaying errors from mkdir if the dir exists
-REM 0.4.0: Moved zip download and extraction to a function and use in case git is unavailable
+REM        is from improved error detection triggering on issues that were already present.
+REM 0.5.0: Updated apegcc version and prevent displaying errors from mkdir if the dir exists.
+REM 0.4.0: Moved zip download and extraction to a function and use in case git is unavailable.
 REM        Also sped up git clone, copied toolchain check from unix shell version and ensured
-REM        calling shell's current directory is unchanged
+REM        calling shell's current directory is unchanged.
 REM 0.3.0: Use alternative musl toolchain prefix for cosmocc compat, transparently pass-through
-REM        parameters to make and remove now unneeded second make step for python tests
-REM 0.2.1: Use a version of ape gcc based on a much newer version of cosmo
+REM        parameters to make and remove now unneeded second make step for python tests.
+REM 0.2.1: Use a version of ape gcc based on a much newer version of cosmo.
 REM 0.2.0: Worked around python test flakiness and cosmo expecting toolchain binaries with
-REM        a specific prefix
+REM        a specific prefix.
 
 if not exist cosmopolitan (
   where git >nul
@@ -23,7 +24,7 @@ if not exist cosmopolitan (
     git clone -c core.autocrlf=false https://github.com/JoshuaWierenga/cosmopolitan --depth=1 -b build-on-windows-fix
   ) else (
     call :download_zip https://github.com/JoshuaWierenga/cosmopolitan/archive/refs/heads/build-on-windows-fix.zip,^
-    build-on-windows-fix.zip, cosmopolitan-build-on-windows-fix, cosmopolitan
+    build-on-windows-fix.zip, "2>nul", cosmopolitan-build-on-windows-fix, cosmopolitan
   )
 )
 setlocal
@@ -54,7 +55,9 @@ exit /b %ERRORLEVEL%
 
 :download_zip
 call curl -LO %~1
-call tar -xvf "%~2"
+REM For some reason tar -xf -v doesn't work but -v is useful for apegcc extraction and a pain for cosmo extraction
+REM so for now I just put 2>nul on the end as required
+call tar -xvf "%~2" %~3
 del "%~2"
-if not "%~3"=="" ren "%~3" "%~4"
+if not "%~4"=="" if not "%~5"=="" ren "%~4" "%~5"
 exit /b 0
--- buildcosmo.sh
+++ buildcosmo.sh
@@ -1,17 +1,19 @@
 #!/bin/sh
 
 # NOTE: Until I fully test things, I recommend using 0.3.0 which uses the build-on-windows-3 branch
-# Joshua's unix cosmopolitan build script 0.4.1
+# Joshua's unix cosmopolitan build script 0.4.2
 # Changes:
+# 0.4.2: Moved zip download and extraction to a function to ensure unzip/bsdtar/tar check is done.
+#        Updated apegcc version.
 # 0.4.1: Fix gcc check.
-# 0.4:   Port some features from the batch version including quicker cosmo download via depth,
+# 0.4.0: Port some features from the batch version including quicker cosmo download via depth,
 #        better gcc setup and remove now unneeded second make step for python tests.
 #        Fixed failure to create the gcc directory not stopping the script.
 #        Use an updated gcc build and an experimental updated cosmo version, the build fails more
 #        but most of that is from improved error detection triggering on already present issues.
-# 0.3:   Use a version of ape gcc based on a much newer version of cosmo, the script now works on
+# 0.3.0: Use a version of ape gcc based on a much newer version of cosmo, the script now works on
 #        linux and netbsd(needs a couple of additional source changes to tests for netbsd though).
-# 0.2:   Worked around gnu tar not supporting zips by checking for unzip and bsdtar first.
+# 0.2.0: Worked around gnu tar not supporting zips by checking for unzip and bsdtar first.
 #        Also worked around cosmo expecting toolchain binaries with a specific prefix and suffix.
 
 # From https://stackoverflow.com/a/45181694
@@ -27,14 +29,27 @@ portable_nproc() {
   echo "$NPROCS"
 }
 
+download_zip() {
+  curl -LO $1
+  if command -v unzip > /dev/null 2>&1; then
+    unzip "$2"
+  elif command -v bsdtar > /dev/null 2>&1; then
+    bsdtar -xvf "$2"
+  else
+    tar -xvf "$2" # hope for bsdtar
+  fi
+  rm "$2"
+  if [ -n "$3" ] && [ -n "$4" ]; then
+    mv "$3" "$4"
+  fi
+}
+
 if [ ! -d cosmopolitan ]; then
   if command -v git > /dev/null 2>&1; then
     git clone https://github.com/JoshuaWierenga/cosmopolitan --depth=1 -b build-on-windows-fix
   else
-    curl -LO https://github.com/JoshuaWierenga/cosmopolitan/archive/refs/heads/build-on-windows-fix.zip
-    tar -xvf build-on-windows-fix.zip
-    rm build-on-windows-fix.zip
-    mv cosmopolitan-build-on-windows-fix/ cosmopolitan
+    download_zip https://github.com/JoshuaWierenga/cosmopolitan/archive/refs/heads/build-on-windows-fix.zip \
+    build-on-windows-fix.zip cosmopolitan-build-on-windows-fix/ cosmopolitan/
   fi
 fi
 cd cosmopolitan || exit
@@ -42,18 +57,11 @@ cd cosmopolitan || exit
 if [ ! -f o/third_party/gcc/bin/x86_64-linux-musl-gcc ]; then
   mkdir -p o/third_party/gcc
   cd o/third_party/gcc || exit
-  curl -LO https://github.com/JoshuaWierenga/RandomScripts/releases/download/z0.0.0-2/gcc11.zip
-  if command -v unzip > /dev/null 2>&1; then
-    unzip gcc11.zip
-  elif command -v bsdtar > /dev/null 2>&1; then
-    bsdtar -xvf gcc11.zip
-  else
-    tar -xvf gcc11.zip # hope for bsdtar
-  fi
-  rm gcc11.zip
+  download_zip https://github.com/JoshuaWierenga/RandomScripts/releases/download/z0.0.0-3/gcc11.zip \
+  gcc11.zip
 
   # Create versions of toolchain programs with the correct prefix and no .com suffix
-  # Cosmo's build system expects x86_64-pc-linux-gnu-{toolname} while the tools
+  # Cosmo's build system expects x86_64-linux-musl-{toolname} while the tools
   # themselves just want {toolname} so this line can't just rename the files
   # From https://unix.stackexchange.com/a/659132
   find bin -type f -exec sh -c 'mv "$1" "${1%.*}"' sh {} \;
