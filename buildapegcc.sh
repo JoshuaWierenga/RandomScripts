e837c640a8de5d69f44917cdda61f0193fb4e287
Joshua Wierenga <joshuagit@joshuawierenga.tech>
2023-09-26 13:33:28 +1000

    Update buildapegcc.sh

--- buildapegcc.sh
+++ buildapegcc.sh
@@ -1,29 +1,30 @@
 #!/bin/sh
 
-# Joshua's ape gcc build script 0.10.0
+# Joshua's ape gcc build script 0.10.1
 # Based on https://github.com/ahgamut/musl-cross-make/blob/cibuild/.github/workflows/release.yml
 # Changes
-#.0.10.0: Update cosmo to a commit from 2023/09/07, there are currently some major
-#         issues with windows support including random crashing so not going any
-#         further for now.
-#         Remove git disable patch since it is no longer needed.
-#0.9.0: Use the current newest version of cosmo from 2023/08/27 🎉
-#       I think a few extra warnings appeared but it does build and work correctly
-#0.8.0: Use a slightly newer version of cosmo from 2023/08/16, update getopt patch
-#0.7.0: Use a slightly newer version of cosmo, cleanup script slightly and remove now
-#       unneeded correction to cosmocc
-#0.6.1: Modify cosmocc so that --update works in this environment
-#0.6.0: Again use a slightly newer version of cosmo, this time from 2023/08/14
-#       Needed to replace setup-cosmos script since it no longer exists
-#0.5.0: Again use a slightly newer version of cosmo, this time from 2023/08/13
-#       Remove chmod on cosmopolitan.a as it is no longer required
-#0.4.0: Updated to a slightly newer version of cosmo with an updated cosmocc
-#       Add fix for cosmocc checking for execute permission on cosmopolitan.a
-#0.3.0: Use a newer version of cosmo from 2023/08/12 instead of one from 2023/07/25
-#       Cleanup cosmo download code slightly by not using a subshell
-#0.2.1: Using same cosmo commit but now from the main repo, manually add getopt change
-#0.2.0: Reverted to an old version of cosmo that has been tested for building gcc
-#       will move back to a newer version once I get that working properly
+#0.10.1: Fix line endings
+#0.10.0: Update cosmo to a commit from 2023/09/07, there are currently some major
+#        issues with windows support including random crashing so not going any
+#        further for now.
+#        Remove git disable patch since it is no longer needed.
+#0.9.0:  Use the current newest version of cosmo from 2023/08/27 🎉
+#        I think a few extra warnings appeared but it does build and work correctly
+#0.8.0:  Use a slightly newer version of cosmo from 2023/08/16, update getopt patch
+#0.7.0:  Use a slightly newer version of cosmo, cleanup script slightly and remove now
+#        unneeded correction to cosmocc
+#0.6.1:  Modify cosmocc so that --update works in this environment
+#0.6.0:  Again use a slightly newer version of cosmo, this time from 2023/08/14
+#        Needed to replace setup-cosmos script since it no longer exists
+#0.5.0:  Again use a slightly newer version of cosmo, this time from 2023/08/13
+#        Remove chmod on cosmopolitan.a as it is no longer required
+#0.4.0:  Updated to a slightly newer version of cosmo with an updated cosmocc
+#        Add fix for cosmocc checking for execute permission on cosmopolitan.a
+#0.3.0:  Use a newer version of cosmo from 2023/08/12 instead of one from 2023/07/25
+#        Cleanup cosmo download code slightly by not using a subshell
+#0.2.1:  Using same cosmo commit but now from the main repo, manually add getopt change
+#0.2.0:  Reverted to an old version of cosmo that has been tested for building gcc
+#        will move back to a newer version once I get that working properly
 
 git clone https://github.com/ahgamut/musl-cross-make.git --depth=1 --branch z0.0.0
 cd musl-cross-make || exit
