d49e18302ec22a467c07c378be16df8010d23dcd
Joshua Wierenga <joshuagit@joshuawierenga.tech>
2024-08-20 19:48:27 +1000

    Fix chibicc not working
    For some reason calling chibicc.exe as chibicc causes it to not output anything.
    Also use cosmocc build of apelink instead of building one.

--- chibicctest/BuildChibicc.bat
+++ chibicctest/BuildChibicc.bat
@@ -1,14 +1,9 @@
 @echo off
 
 setlocal
-:: set PATH=%~dp0cosmo\build\bootstrap;%~dp0bin;%PATH%
 set PATH=%~dp0bin;%PATH%
 cd cosmo
 
-:: dash build/download-cosmocc.sh $(COSMOCC) 3.6.2 268aa82d9bfd774f76951b250f87b8edcefd5c754b8b409e1639641e8bd8d5b
 if not exist o\third_party\chibicc\chibicc dash ../batchtodash.sh make SHELL=dash -j17 o//third_party/chibicc/chibicc
-if not exist o\tool\build\apelink dash ../batchtodash.sh make SHELL=dash -j17 o//tool/build/apelink
-
 if not exist ..\bin\chibicc.exe copy o\third_party\chibicc\chibicc ..\bin\chibicc.exe
-if not exist ..\bin\apelink.exe copy o\tool\build\apelink ..\bin\apelink.exe
 endlocal
--- chibicctest/DownloadCosmo.bat
+++ chibicctest/DownloadCosmo.bat
@@ -8,14 +8,10 @@ cd bin
 set COSMOS_URL=https://cosmo.zip/pub/cosmos/v/3.7.1/bin
 set COSMOSWINFDSHANGFIX_URL=https://github.com/UTAS-Programming-Club/UntitledTextAdventure/raw/1634bd6/build/bootstrap
 
-:: if not exist dash curl -fLo dash %COSMOS_URL%/dash
 if not exist dash curl -fLo dash %COSMOSWINFDSHANGFIX_URL%/sh
 if not exist make curl -fLo make %COSMOSWINFDSHANGFIX_URL%/make
 if not exist pwd curl -fLo pwd %COSMOS_URL%/pwd
-:: if not exist sha256sum curl -fLo sha256sum %COSMOS_URL%/sha256sum
 if not exist uname curl -fLo uname %COSMOS_URL%/uname.ape
-:: if not exist unzip curl -fLo unzip %COSMOS_URL%/unzip
-:: if not exist wget curl -fLo wget %COSMOS_URL%/wget
 
 if not exist dash.exe copy dash dash.exe
 
@@ -29,5 +25,6 @@ if not exist cosmocc.zip curl -fLo cosmocc.zip https://github.com/jart/cosmopoli
 if not exist cosmo\.cosmocc\3.6.2 mkdir cosmo\.cosmocc\3.6.2
 if not exist cosmo\.cosmocc\3.6.2\bin\x86_64-unknown-cosmo-cc tar -xvf cosmocc.zip -C cosmo\.cosmocc\3.6.2
 
+if not existbin\apelink.exe copy .cosmocc\3.6.2\bin\apelink bin\apelink.exe
 if not exist bin\gzip copy cosmo\.cosmocc\3.6.2\bin\gzip.ape bin\gzip
 if not exist bin\mkdir copy cosmo\.cosmocc\3.6.2\bin\mkdir.ape bin\mkdir
