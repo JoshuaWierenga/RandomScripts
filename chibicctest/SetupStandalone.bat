98a3bc061884c1f57b2b31f8dbfe2940b0fb45f7
Joshua Wierenga <joshuagit@joshuawierenga.tech>
2024-08-20 22:54:05 +1000

    Only use x86_64 files for toolchain
    Chibicc only does codegen for x86_64 so no
    need to have aarch64 execs in the toolchain.

--- chibicctest/SetupStandalone.bat
+++ chibicctest/SetupStandalone.bat
@@ -5,7 +5,16 @@ copy bin\apelink.exe out\bin\apelink.com
 copy bin\chibicc.exe out\bin\chibicc.com
 copy bin\dash.exe out\bin\
 
-xcopy cosmo\.cosmocc\3.6.2\ out\cosmo\.cosmocc\3.6.2\ /sy
+xcopy cosmo\.cosmocc\3.6.2\ out\cosmo\.cosmocc\3.6.2\ /y
+mkdir cosmo\.cosmocc\3.6.2\bin\
+copy cosmo\.cosmocc\3.6.2\bin\apelink out\cosmo\.cosmocc\3.6.2\bin\
+copy cosmo\.cosmocc\3.6.2\bin\ape-x86_64.elf out\cosmo\.cosmocc\3.6.2\bin\
+copy cosmo\.cosmocc\3.6.2\bin\fixupobj out\cosmo\.cosmocc\3.6.2\bin\
+xcopy cosmo\.cosmocc\3.6.2\bin\unknown-unknown-cosmo-* out\cosmo\.cosmocc\3.6.2\bin\ /y
+xcopy cosmo\.cosmocc\3.6.2\bin\x86_64-linux-cosmo-* out\cosmo\.cosmocc\3.6.2\bin\ /y
+xcopy cosmo\.cosmocc\3.6.2\include\ out\cosmo\.cosmocc\3.6.2\include\ /sy
+xcopy cosmo\.cosmocc\3.6.2\libexec\gcc\x86_64-linux-cosmo out\cosmo\.cosmocc\3.6.2\libexec\gcc\x86_64-linux-cosmo\ /sy
+xcopy cosmo\.cosmocc\3.6.2\x86_64-linux-cosmo\ out\cosmo\.cosmocc\3.6.2\x86_64-linux-cosmo\ /sy
 xcopy cosmo\ctl\*.h out\cosmo\ctl\ /sy
 xcopy cosmo\dsp\*.h out\cosmo\dsp\ /sy
 xcopy cosmo\libc\*.h out\cosmo\libc\ /sy
