296c5daa12708fe276a12e436316108bec45e971
Joshua Wierenga <joshuagit@joshuawierenga.tech>
2024-08-20 23:28:04 +1000

    Remove rest of unused execs from toolchain

--- chibicctest/SetupStandalone.bat
+++ chibicctest/SetupStandalone.bat
@@ -7,17 +7,13 @@ copy bin\dash.exe out\bin\
 
 xcopy cosmo\.cosmocc\3.6.2\ out\cosmo\.cosmocc\3.6.2\ /y
 mkdir out\cosmo\.cosmocc\3.6.2\bin\
-copy cosmo\.cosmocc\3.6.2\bin\apelink out\cosmo\.cosmocc\3.6.2\bin\
 copy cosmo\.cosmocc\3.6.2\bin\ape-x86_64.elf out\cosmo\.cosmocc\3.6.2\bin\
 copy cosmo\.cosmocc\3.6.2\bin\fixupobj out\cosmo\.cosmocc\3.6.2\bin\
-xcopy cosmo\.cosmocc\3.6.2\bin\unknown-unknown-cosmo-* out\cosmo\.cosmocc\3.6.2\bin\ /y
-xcopy cosmo\.cosmocc\3.6.2\bin\x86_64-unknown-cosmo-* out\cosmo\.cosmocc\3.6.2\bin\ /y
-xcopy cosmo\.cosmocc\3.6.2\bin\x86_64-linux-cosmo-* out\cosmo\.cosmocc\3.6.2\bin\ /y
+copy cosmo\.cosmocc\3.6.2\bin\x86_64-linux-cosmo-gcc out\cosmo\.cosmocc\3.6.2\bin\
+copy cosmo\.cosmocc\3.6.2\bin\x86_64-unknown-cosmo-cc out\cosmo\.cosmocc\3.6.2\bin\
 xcopy cosmo\.cosmocc\3.6.2\include\ out\cosmo\.cosmocc\3.6.2\include\ /sy
 mkdir out\cosmo\.cosmocc\3.6.2\libexec\gcc\x86_64-linux-cosmo\14.1.0\
-copy cosmo\.cosmocc\3.6.2\libexec\gcc\x86_64-linux-cosmo\14.1.0\as out\cosmo\.cosmocc\3.6.2\libexec\gcc\x86_64-linux-cosmo\14.1.0\
 copy cosmo\.cosmocc\3.6.2\libexec\gcc\x86_64-linux-cosmo\14.1.0\ld out\cosmo\.cosmocc\3.6.2\libexec\gcc\x86_64-linux-cosmo\14.1.0\
-copy cosmo\.cosmocc\3.6.2\libexec\gcc\x86_64-linux-cosmo\14.1.0\ld.bfd out\cosmo\.cosmocc\3.6.2\libexec\gcc\x86_64-linux-cosmo\14.1.0\
 xcopy cosmo\.cosmocc\3.6.2\x86_64-linux-cosmo\ out\cosmo\.cosmocc\3.6.2\x86_64-linux-cosmo\ /sy
 xcopy cosmo\ctl\*.h out\cosmo\ctl\ /sy
 xcopy cosmo\dsp\*.h out\cosmo\dsp\ /sy
